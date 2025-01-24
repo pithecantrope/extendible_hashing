53562dab781b73b5bb6b644ea52b4c684ec09236
pithecantrope <fartdraft@gmail.com>
2025-01-25 01:07:05 +0300

    Format main ; fix buffer tokenizer loop

--- src/main.c
+++ src/main.c
@@ -5,19 +5,19 @@
 #define FNV_OFFSET 14695981039346656037ULL
 #define FNV_PRIME  1099511628211ULL
 
-size_t
+EH_HASH_T
 hash_fnv1a(const void* key) {
-    size_t hash = FNV_OFFSET;
-    for (const char* p = key; *p; p++) {
-        hash ^= (size_t)*p;
-        hash *= FNV_PRIME;
-    }
-    return hash;
+        EH_HASH_T hash = FNV_OFFSET;
+        for (const char* p = key; *p; p++) {
+                hash ^= (EH_HASH_T)*p;
+                hash *= FNV_PRIME;
+        }
+        return hash;
 }
 
 int
 cmp(const void* key1, const void* key2) {
-    return strcmp(key1, key2);
+        return strcmp(key1, key2);
 }
 
 #define BUFFER_SIZE 4096
@@ -25,46 +25,43 @@ cmp(const void* key1, const void* key2) {
 
 int
 main(int argc, char* argv[]) {
-    unsigned bucket_capacity;
-    if (argc != 2 || sscanf(argv[1], "%u", &bucket_capacity) != 1) {
-        fprintf(stderr, "Usage: %s <bucket_capacity>\n", *argv);
-        return EXIT_FAILURE;
-    }
-
-    const char path[] = "res/data.txt";
-    FILE* file = fopen(path, "rt");
-    if (file == NULL) {
-        fprintf(stderr, "Could not open %s for reading\n", path);
-        return EXIT_FAILURE;
-    }
-
-    const char* word;
-    eh_hashtable_t* const table = eh_create(WORD_SIZE, sizeof(size_t), bucket_capacity, hash_fnv1a, cmp);
-    char buffer[BUFFER_SIZE];
-    while (fgets(buffer, BUFFER_SIZE, file) != NULL) {
-        word = strtok(buffer, "\n");
-        const void* const value = eh_lookup(table, word);
-        if (value == NULL) {
-            eh_insert(table, word, &(size_t){1});
-        } else {
-            ++*(size_t*)value;
+        unsigned bucket_capacity;
+        if (argc != 2 || sscanf(argv[1], "%u", &bucket_capacity) != 1) {
+                fprintf(stderr, "Usage: %s <bucket_capacity>\n", *argv);
+                return EXIT_FAILURE;
         }
 
-        while (word != NULL) {
-            word = strtok(NULL, "\n");
+        const char path[] = "res/data.txt";
+        FILE* const file = fopen(path, "rt");
+        if (file == NULL) {
+                fprintf(stderr, "Could not open %s for reading\n", path);
+                return EXIT_FAILURE;
         }
-    }
 
-    size_t count = 0;
-    const void *key, *val;
-    for (eh_iterator_t iterator = eh_iter(table); eh_next(&iterator, &key, &val);) {
-        if (*(size_t*)val >= 1024) {
-            ++count;
+        eh_hashtable_t* const table = eh_create(WORD_SIZE, sizeof(EH_HASH_T), bucket_capacity,
+                                                hash_fnv1a, cmp);
+        for (char buffer[BUFFER_SIZE]; fgets(buffer, BUFFER_SIZE, file) != NULL;) {
+                for (const char* word = strtok(buffer, "\n"); word != NULL;
+                     word = strtok(NULL, "\n")) {
+                        const void* const value = eh_lookup(table, word);
+                        if (value == NULL) {
+                                eh_insert(table, word, &(EH_HASH_T){1});
+                        } else {
+                                ++*(EH_HASH_T*)value;
+                        }
+                }
         }
-    }
-    printf("%zu words with >= 1024 repetitions\n", count);
 
-    eh_destroy(table);
-    fclose(file);
-    return EXIT_SUCCESS;
+        size_t count = 0;
+        const void *key, *val;
+        for (eh_iterator_t iterator = eh_iter(table); eh_next(&iterator, &key, &val);) {
+                if (*(EH_HASH_T*)val >= 1024) {
+                        ++count;
+                }
+        }
+        printf("%zu words with >= 1024 repetitions\n", count);
+
+        eh_destroy(table);
+        fclose(file);
+        return EXIT_SUCCESS;
 }
